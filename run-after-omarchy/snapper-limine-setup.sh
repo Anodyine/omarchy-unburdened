0a7daa2d3696482357c098e2fa991f7a615b140f
Kyle Merritt <ky562730@ucf.edu>
2025-11-07 17:38:57 -0500

    updated directory names

--- run-after-omarchy/snapper-limine-setup.sh
+++
@@ -1,5 +0,0 @@
-sudo pacman -S snapper btrfs-progs limine-snapper-sync
-sudo snapper -c root create-config /
-sudo systemctl enable --now snapper-timeline.timer snapper-cleanup.timer >/dev/null
-sudo snapper -c root create -d "Initial snapshot" >/dev/null
-sudo snapper -c root list
